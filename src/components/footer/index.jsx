7a9f32b86d17f64e92e004f36d1344d870fbe7bc
Barros Lucas <lucasbarros78@hotmail.com>
2023-08-19 20:54:24 -0300

    home finalizado

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,15 +1,22 @@
 import styles from './footer.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebook, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons'
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
         <footer className={styles.footerContainer}>
             <p>Lun a Vie de 8 a 14 hs</p>
             <div className={styles.iconsContainer}>
+            <Link to={'https://www.facebook.com/people/Centro-de-D%C3%ADa-La-Novena/100066680851586/'} target="_blank">
                 <FontAwesomeIcon icon={faFacebook}/>
+            </Link>
+            <Link to={'https://www.instagram.com/centro2104'} target="_blank" >
                 <FontAwesomeIcon icon={faInstagram}/>
+            </Link>
+            <Link to={'https://wa.me/2995350417'} target="_blank">
                 <FontAwesomeIcon icon={faWhatsapp}/>
+            </Link>
             </div>
             <p>Entre Ríos 450 - Bº Otaño</p>
         </footer>
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,27 @@
 
 import styles from './home.module.css'
+import salud from '../../assets/images/mentalHealt.jpg'
+import juguete from '../../assets/images/juguetes.jpg'
 
 const Home = () => {
     return (
         <main className={styles.homeContainer}>
-            <div>
-            
+        <div className={styles.cardContainer}>
+            <img src={salud}/>
+            <div className={styles.glass}></div>
+            <div className={styles.infoContainer}>
+                <p>Centro de día “La Novena”: Seis años de compromiso y dedicación en el abordaje integral y acompañamiento a personas...</p>
+                <button>Ver mas</button>
             </div>
+        </div>
+        <div className={styles.cardContainer}>
+            <img src={juguete}/>
+            <div className={styles.glass}></div>
+            <div className={styles.infoContainer}>
+                <p>VENTA DE JUGUETES: <br/>Fabricados en madera por los usuarios del centro de día</p>
+                <button>Ver mas</button>
+            </div>
+        </div>
         </main>
     );
 }
