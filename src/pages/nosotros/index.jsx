7a6cacb0cfb80ee7122c36d4f89d2f44218939ea
Barros Lucas <lucasbarros78@hotmail.com>
2023-08-29 15:13:39 -0300

    actualización de data, secciòn nosotros modificada

--- src/pages/nosotros/index.jsx
+++ src/pages/nosotros/index.jsx
@@ -1,4 +1,7 @@
 import manos from '../../assets/images/manos.png'
+import user1 from '../../assets/images/users/users1.jpeg'
+import user2 from '../../assets/images/users/users2.jpeg'
+import user3 from '../../assets/images/users/users3.jpeg'
 import styles from './nosotros.module.css'
 
 const Nosotros = () => {
@@ -9,12 +12,29 @@ const Nosotros = () => {
                 <h2 >Nosotros</h2>
             </div>
                 <div className={styles.infoContainer}>
+                <div className={styles.imgContainer}>
+                    <div className={styles.uno}>
+                        <img  src={user3}/>
+                    </div>
+                    <div className={styles.dos}>
+                        <img src={user2}/>
+                    </div>
+                    <div className={styles.tres}>
+                        <img src={user1}/>
+                    </div>
+                </div>
+                    <div className={styles.textInfo}>
+                        <p>Somos usuarias/os del Centro de día que elaboramos juguetes didácticos.</p>
+                        <p>Participamos de ferias en donde vendemos nuestros productos, también realizamos juguetes a pedido y en nuestro catálogo podrán encontrar los juguetes disponibles.</p>
+                    </div>
+                </div>
+                <div className={styles.infoContainer}>
                     <img src={manos}/>
                     <div className={styles.textInfo}>
                         <p>Seis años de compromiso y dedicación en el abordaje integral y acompañamiento a personas con padecimiento subjetivo y/o consumo problemático. 
                         Desde su creación, este dispositivo se ha convertido en un espacio de apoyo para la comunidad, brindando una atención especializada y contención emocional a quienes lo necesitan.</p>
                         <p>Ubicado en la calle Entre Ríos N° 450 del barrio Otaño, el Centro de Día “La Novena” es parte esencial del servicio de Salud Mental del hospital local. Su papel como complemento de la red de atención de salud ha sido fundamental en la atención a pacientes con problemas de salud mental y consumo en Plaza Huincul y Cutral Co.</p>
-                        <p>Durante seis años de trayectoria, el Centro ha destacado por contar con un equipo interdisciplinario de salud comprometido y dedicado. Psicólogos, trabajadores sociales, psiquiatras y otros especialistas han trabajado en conjunto para ofrecer diversos talleres.</p>
+                        <p>Durante seis años de trayectoria, el Centro ha destacado por contar con un equipo interdisciplinario de salud comprometido y dedicado. Psicólogos, trabajadores sociales, acompañantes terapeúticos y enfermeros ademas de los talleristas que trabajan en conjunto para ofrecer diversos talleres.</p>
                     </div>
                 </div>
             </section>
